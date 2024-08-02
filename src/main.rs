4e1d7295b7b2abb36a8ae8f2a91ad60a2834b425
SirIridium <127507341+SirIridium@users.noreply.github.com>
2024-08-02 08:54:39 -0600

    almost finished

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,70 @@
 use leptos::*;
 
 #[component]
-fn App() -> impl IntoView {
+fn header() -> impl IntoView {
     view! {
         <div class="header">
             <h1>"LodgeMaster Social Page"</h1>
+            <hr></hr>
         </div>
+    }
+}
+
+#[component]
+fn calendar() -> impl IntoView {
+    view! {
+        <h2>"Calendar"</h2>
+        <table>
+            <tr>
+                <th>"Sunday"</th>
+                <th>"Monday"</th>
+                <th>"Tuesday"</th>
+                <th>"Wednesday"</th>
+                <th>"Thursday"</th>
+                <th>"Friday"</th>
+                <th>"Saturday"</th>
+            </tr>
+            {(0..5).into_iter().map(|_| view! {
+                <tr>
+                {(0..7).into_iter().map(|_| view! {
+                    <td>
+                    </td>
+                }).collect::<Vec<_>>()}
+                </tr>
+            }).collect::<Vec<_>>()}
+        </table>
+
+    }
+}
+
+#[component]
+fn points() -> impl IntoView {
+    view! {
+    <h2>Points</h2>
+    <p>Attend more events for more points in your lodge!</p>
+
+    }
+}
+
+#[component]
+fn app() -> impl IntoView {
+    view! {
+        <Header/>
+        <div class="name">
+            <h1> "Luke Bushell" </h1>
+            <p>"Gabriel Honor"</p>
+        </div>
+
+        <Calendar/>
+        <Points/>
+        <div class="side-by-side">
+
+        <div class="upcoming">
+        <h2>"Upcoming Events"</h2>
+        </div>
+
+        </div>
+
     }
 }
 
